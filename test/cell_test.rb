fa2de78ce2389cdd23bc8c4e7e3d6245e6957600
John Travers <johnk.travers@gmail.com>
2019-08-20 17:20:09 -0600

    Add cell class and cell test
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

---
+++ test/cell_test.rb
@@ -0,0 +1,45 @@
+require './lib/ship'
+require './lib/cell'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class CellTest < Minitest::Test
+
+  def setup
+    @cell = Cell.new('B4')
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Cell, @cell
+  end
+
+  def test_cell_has_a_coordinate
+    assert_equal 'B4', @cell.coordinate
+  end
+
+  def test_cell_starts_without_a_ship
+    assert_nil @cell.ship
+  end
+
+  def test_cell_starts_empty
+    assert_equal true, @cell.empty?
+  end
+
+  def test_it_can_contain_a_ship
+    @cell.place_ship(@cruiser)
+
+    assert_equal @cruiser, @cell.ship
+  end
+
+  def test_placed_ship_is_a_ship
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_cell_with_ship_is_not_empty
+    @cell.place_ship(@cruiser)
+
+    assert_equal false, @cell.empty?
+  end
+
+end
