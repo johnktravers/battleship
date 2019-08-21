77e03674e7c2cb318bca8d55a48358eca9ba325b
Scott Regenthal <scottregenthal@gmail.com>
2019-08-21 14:09:35 -0600

    Initialize board class
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>

---
+++ test/board_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def setup
+    @board = Board.new
+  end
+
+  def test_it_exists
+    assert_instance_of Board, @board
+  end
+
+end
