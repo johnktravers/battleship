9748d764d5105d749fef78488d841267776169af
John Travers <johnk.travers@gmail.com>
2019-08-25 16:57:42 -0600

    Add ship placement for computer
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,8 +1,13 @@
 class Computer
-  attr_reader :board
+  attr_reader :board, :ships
 
   def initialize(board)
     @board = board
+    @ships = []
+  end
+
+  def add_ship(ship)
+    @ships.push(ship)
   end
 
   def select_random_coord
@@ -10,14 +15,31 @@ class Computer
   end
 
   def create_random_coord_array(ship)
-
-    random_array = @board.cells.keys.sort.sample(ship.length)
-
-    until @board.valid_placement?(ship, random_array)
-      random_array = @board.cells.keys.sort.sample(ship.length)
+    loop do
+      random_array = @board.cells.keys.sample(ship.length)
+      if @board.valid_placement?(ship, random_array)
+        break random_array
+      end
     end
+  end
 
-    random_array
+  def place_ships
+    @ships.each do |ship|
+      @board.place(ship, create_random_coord_array(ship))
+    end
+  end
+
+  def prompt_player
+    nums = { 2 => "two", 3 => "three"}
+
+    message = "I have laid out my ships on the grid.\n" +
+              "You now need to lay out your #{nums[@ships.length]} ships:\n"
+
+    @ships.each do |ship|
+      message.concat("  The #{ship.name} is #{nums[ship.length]} units long.\n")
+    end
+    print message
+    message
   end
 
 end
