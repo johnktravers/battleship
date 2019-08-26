4f71f88a683af8aa0c8c22cf387c039464a6a34b
Scott Regenthal <scottregenthal@gmail.com>
2019-08-25 19:19:40 -0600

    Add loop to continue game play until both ships are sunk
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>

--- lib/runner.rb
+++ lib/runner.rb
@@ -10,8 +10,8 @@ submarine = Ship.new("Submarine", 2)
 player_board = Board.new
 computer_board = Board.new
 
-computer = Computer.new(computer_board)
-player = Player.new(player_board)
+computer = Computer.new(computer_board, player_board)
+player = Player.new(computer_board, player_board)
 game = Game.new(computer_board, player_board)
 
 game.main_menu
@@ -27,5 +27,13 @@ player.add_ship(cruiser)
 player.add_ship(submarine)
 
 player.present_board
+
+
+until computer_board.render.count("X") == 5 || player_board.render.count("X") == 5
+  game.display_boards
+
+  player.fire_upon_coord
+  computer.fire_upon_coord
+end
 
 game.display_boards
