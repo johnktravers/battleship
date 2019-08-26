4616242d9555a422059b85330ed0a59aa0d5aac3
Scott Regenthal <scottregenthal@gmail.com>
2019-08-25 18:02:27 -0600

    Add main menu functionality and display boards method
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,31 @@
 class Game
+  attr_reader :computer_board, :player_board
 
-  def main_menu
-    "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit."
+  def initialize(computer_board, player_board)
+    @computer_board = computer_board
+    @player_board = player_board
   end
 
+  def main_menu
+    print "Welcome to BATTLESHIP\nEnter p to play. Enter q to quit.\n> "
 
+    input = gets.chomp
+    loop do
+      if input == "p"
+        break
+      elsif input == "q"
+        abort
+      else
+        print "Please enter either p or q to continue.\n> "
+        input = gets.chomp
+      end
+    end
+  end
+
+  def display_boards
+    print display = "=============COMPUTER BOARD=============\n" +
+                    @computer_board.render +
+                    "==============PLAYER BOARD==============\n" +
+                    @player_board.render(true)
+  end
 end
