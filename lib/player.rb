a22bbc1b3bc41fa287ba0d1db267bb57ac3bfebc
John Travers <johnk.travers@gmail.com>
2019-08-26 22:38:20 -0600

    Add whitespace to gameplay

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,9 @@
 class Player
-  attr_reader :computer_board, :player_board, :computer_ships, :player_ships
+  attr_reader :computer_board,
+              :player_board,
+              :computer_ships,
+              :player_ships,
+              :shot_coords
 
   def initialize(computer_board, player_board)
     @computer_board = computer_board
@@ -20,42 +24,50 @@ class Player
   def present_board
     @player_ships.each do |ship|
       message = "Enter the squares for the #{ship.name} (#{ship.length} spaces):\n> "
-      print @player_board.render(true) + message
-      coords = gets.chomp.split
+      print @player_board.render(true) + "\n" + message
+
+      coords = gets.chomp.upcase.split
+      print "\n"
+
       loop do
         if @player_board.valid_placement?(ship, coords)
           @player_board.place(ship, coords)
           break
         else
           print "Those are invalid coordinates. Please try again:\n> "
-          coords = gets.chomp.split
+          coords = gets.chomp.upcase.split
+          print "\n"
         end
       end
-      print "\n"
     end
+    system "clear"
   end
 
   def fire_upon_coord
     print "Enter the coordinate for your shot:\n> "
-    coord = gets.chomp
+    coord = gets.chomp.upcase
+    print "\n"
+
     loop do
       if @computer_board.valid_coordinate?(coord)
         if !@shot_coords.include?(coord)
           @computer_board.cells[coord].fire_upon
           @shot_coords.push(coord)
+          system "clear"
           display_result(coord)
           break
         else
           print "That coordinate has already been fired upon.\n" +
                 "Please enter a different coordinate:\n> "
-          coord = gets.chomp
+          coord = gets.chomp.upcase
+          print "\n"
         end
       else
         print "Please enter a valid coordinate:\n> "
-        coord = gets.chomp
+        coord = gets.chomp.upcase
+        print "\n"
       end
     end
-    print "\n"
   end
 
   # Helper method
