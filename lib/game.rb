6cddec54d134bc114d49e7bb81fd255de85382f1
John Travers <johnk.travers@gmail.com>
2019-08-27 12:02:55 -0600

    Add play method to game class and remove unnecessary tests
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,14 @@
 class Game
-  attr_reader :computer_board, :player_board
+  attr_reader :computer_board,
+              :player_board,
+              :computer_ships,
+              :player_ships
 
-  def initialize(computer_board, player_board)
-    @computer_board = computer_board
-    @player_board = player_board
-    @player_ships = []
+  def initialize
+    @computer_board = nil
+    @player_board = nil
     @computer_ships = []
+    @player_ships = []
   end
 
   def main_menu
@@ -48,5 +51,53 @@ class Game
     print "Press Enter key to continue:\n> "
     gets
     system "clear"
+  end
+
+  def play
+    loop do
+      computer_cruiser = Ship.new("Cruiser", 3)
+      computer_submarine = Ship.new("Submarine", 2)
+      player_cruiser = Ship.new("Cruiser", 3)
+      player_submarine = Ship.new("Submarine", 2)
+
+      @player_board = Board.new
+      @computer_board = Board.new
+
+      computer = Computer.new(@computer_board, @player_board)
+      player = Player.new(@computer_board, @player_board)
+
+      main_menu
+
+      # Computer setup
+      computer.add_computer_ship(computer_cruiser)
+      computer.add_computer_ship(computer_submarine)
+      computer.place_ships
+
+      computer.add_player_ship(player_cruiser)
+      computer.add_player_ship(player_submarine)
+      computer.prompt_player
+
+      # Player setup
+      player.add_player_ship(player_cruiser)
+      player.add_player_ship(player_submarine)
+
+      # Provide access to opponents' ships
+      player.add_computer_ship(computer_cruiser)
+      player.add_computer_ship(computer_submarine)
+
+      player.present_board
+
+
+      until @computer_board.render.count("X") == 5 || @player_board.render.count("X") == 5
+        display_boards
+
+        player.fire_upon_coord
+        computer.fire_upon_coord
+      end
+
+      display_boards
+
+      game_over
+    end
   end
 end
