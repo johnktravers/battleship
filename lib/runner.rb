ce38a87f589d580e38481f110e7b9f4a6b1b56a1
John Travers <johnk.travers@gmail.com>
2019-08-26 22:45:38 -0600

    Add loop to runner file

--- lib/runner.rb
+++ lib/runner.rb
@@ -5,49 +5,49 @@ require './lib/player'
 require './lib/game'
 require './lib/computer'
 
-computer_cruiser = Ship.new("Cruiser", 3)
-computer_submarine = Ship.new("Submarine", 2)
-player_cruiser = Ship.new("Cruiser", 3)
-player_submarine = Ship.new("Submarine", 2)
+loop do
+  computer_cruiser = Ship.new("Cruiser", 3)
+  computer_submarine = Ship.new("Submarine", 2)
+  player_cruiser = Ship.new("Cruiser", 3)
+  player_submarine = Ship.new("Submarine", 2)
 
-player_board = Board.new
-computer_board = Board.new
+  player_board = Board.new
+  computer_board = Board.new
 
-computer = Computer.new(computer_board, player_board)
-player = Player.new(computer_board, player_board)
-game = Game.new(computer_board, player_board)
+  computer = Computer.new(computer_board, player_board)
+  player = Player.new(computer_board, player_board)
+  game = Game.new(computer_board, player_board)
 
-game.main_menu
+  game.main_menu
 
-# Computer setup
-computer.add_computer_ship(computer_cruiser)
-computer.add_computer_ship(computer_submarine)
-computer.place_ships
+  # Computer setup
+  computer.add_computer_ship(computer_cruiser)
+  computer.add_computer_ship(computer_submarine)
+  computer.place_ships
 
-computer.add_player_ship(player_cruiser)
-computer.add_player_ship(player_submarine)
-computer.prompt_player
+  computer.add_player_ship(player_cruiser)
+  computer.add_player_ship(player_submarine)
+  computer.prompt_player
 
-# Player setup
-player.add_player_ship(player_cruiser)
-player.add_player_ship(player_submarine)
+  # Player setup
+  player.add_player_ship(player_cruiser)
+  player.add_player_ship(player_submarine)
 
-# Provide access to opponents' ships
-player.add_computer_ship(computer_cruiser)
-player.add_computer_ship(computer_submarine)
+  # Provide access to opponents' ships
+  player.add_computer_ship(computer_cruiser)
+  player.add_computer_ship(computer_submarine)
 
-player.present_board
+  player.present_board
 
 
-until computer_board.render.count("X") == 5 || player_board.render.count("X") == 5
+  until computer_board.render.count("X") == 5 || player_board.render.count("X") == 5
+    game.display_boards
+
+    player.fire_upon_coord
+    computer.fire_upon_coord
+  end
+
   game.display_boards
 
-  player.fire_upon_coord
-  computer.fire_upon_coord
+  game.game_over
 end
-
-game.display_boards
-
-game.game_over
-
-game.main_menu
